4e431a1ce4c72400d3b41e76bd775a19ea944c7c
MineGamer69 <aaryankapoor2003@gmail.com>
2023-02-07 18:57:39 -0500

    Step2: added an option to add or subtract

--- calculator.java
+++ calculator.java
@@ -6,10 +6,16 @@ public class calculator{
         int num1 = inp.nextInt();
         System.out.println("Enter 2nd Number: ");
         int num2 = inp.nextInt();
-        int sum = num1 + num2;
-        System.out.println("Result = " + sum);
+        System.out.println("Would you like to 1) Add 2) Subtract");
+        int option = inp.nextInt();
 
-
-
+        if(option == 1){
+            int addSum = num1 + num2;
+            System.out.println("Result = " + addSum);
+        }
+        if(option == 2){
+            int subSum = num1 - num2;
+            System.out.println("Result = " + subSum);
+        }
     }
 }
